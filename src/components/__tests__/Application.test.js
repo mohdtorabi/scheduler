57fbb6b8164844ed950993956594ec5e45609f66
Mohammad <mitq8.mt@gmail.com>
2020-12-16 16:03:20 -0800

    cypress test is done

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -32,11 +32,10 @@ it("loads data, books an interview and reduces the spots remaining for Monday by
   fireEvent.click(getByText(appointment, "Save"));
 
   expect(getByText(appointment, "Saving")).toBeInTheDocument();
-  
-    await waitForElement(()=> getByText(appointment, "Lydia Miller-Jones"));
 
-    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday")
-  );
+  await waitForElement(()=> getByText(appointment, "Lydia Miller-Jones"));
+  const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday")
+    );
   expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 
 });
